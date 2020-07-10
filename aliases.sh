c3a91fecb9fbd7a69ae49549b0ace0054e8d842d
rronan <ronan@milvue.com>
2020-07-10 13:29:35 +0200

    add aliases

---
+++ aliases.sh
@@ -0,0 +1,31 @@
+#Trick to open vim a the line of bug (python)
+function File {
+    FILE="$1"
+    LINE="$3"
+    nvim "${FILE:0:-1}" +"${LINE:0:-1}"
+}
+
+export EDITOR="nvim"
+
+alias vup="nvim +PlugUpgrade +PlugUpdate"
+alias zprezto="$EDITOR ~/.zprezto"
+alias zshrc="$EDITOR ~/.zshrc"
+alias zsource='source ~/.zshrc'
+
+
+#RIP ptpb
+#alias ptpb="curl -F c=@- https://ptpb.pw/ | sed '/^url:/ s/$/.jpeg/'"
+
+bindkey "^[[A" history-substring-search-up
+bindkey "^[[B" history-substring-search-down
+bindkey -s "^K" "^[[A"
+bindkey -s "^J" "^[[B"
+bindkey -s "^@" "^M"
+#bindkey -s "^F" " fg^M ^M"
+bindkey -s '^[z' ' ^u fg\n'
+
+alias vim='nvim'
+alias imgcat='/usr/local/bin/imgcat imgcat'
+
+zstyle ':completion:*:directory-stack' list-colors '=(#b) #([0-9]#)*( *)==1;34=1;36'
+
